0cbf60c1f39401cdc21ec246e7a2c2fba536cc96
Thomas GEORGE <thomas.george@itemm.fr>
2022-11-10 16:38:26 +0100

    added style to look like original chart

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 // no conflict mode in jQuery
 var $j = jQuery.noConflict();
 
+// validate numerical input
+function checkNumInput(value) {
+    const result = Number.parseFloat(value);
+    if (isNaN(result)) {
+        alert("please enter a valid input value");
+        return false;
+    } else {
+        // alert("enter is a valid input value !");
+        return true;
+    }
+}
+
 // don't do anything until page is loaded : 
 $j(document).ready(function () {
     
@@ -10,4 +22,5 @@ $j(document).ready(function () {
         $j(this).hide();
     });
 
+    $j()
 });
