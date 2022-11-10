debb3348181a4b47da027c4a8f1256b535774bb8
Thomas GEORGE <thomas.george@itemm.fr>
2022-11-10 12:22:52 +0100

    working base for OUTILS_LAME_IDIOPHONE inside WP

---
+++ includes/html_strings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * These are the 'HTML string' constants returned by the shorcodes in WP pages.
+ * 
+ * NOTE : Please use double quotes " " to add any attributes in the HTML string 
+ * (which is surrounded by simple quotes ' ') to not break the PHP string syntax.
+ */
+
+define('OUTIL_LAMES_IDIOPHONES','
+    <!-- -->
+    <h1 class="olt-title">OUTIL LAMES IDIOPHONES</h1>
+');
---
+++ openlab-tools.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+Plugin Name: openlab-tools
+Plugin URI: https://www.totgeogeo.com
+Description: This plugin displays the Openlab tools in WP pages. They are called by shortcodes [/] in the WP pages.
+Version: 1.0
+Author: Thomas GEORGE for ITEMM / OpenLab
+*/
+
+// includes
+require_once(plugin_dir_path( __FILE__ ) . 'includes/html_strings.php');
+
+// functions to enqueue Stylesheets and Scripts
+function olt_styles() {
+    wp_enqueue_style( 'olt-style',  plugin_dir_url( __FILE__ ) . '/css/style.css' );                      
+}
+
+function olt_scripts() {
+    wp_enqueue_script( 'olt-script',  plugin_dir_url( __FILE__ ) . '/js/script.js' );                      
+}
+
+/**
+ * The olt_app is loaded inside a div.
+ * We can use it on the admin page doing the sortcode [openlab-tools]
+ * @return false|string
+ */
+
+function olt_app(){
+
+    // enqueue .CSS & .JS
+    olt_styles();
+    olt_scripts();
+
+    // return the HTML content as a string
+    return OUTIL_LAMES_IDIOPHONES;
+
+}
+
+add_shortcode( 'openlab-tools', 'olt_app' );
+
