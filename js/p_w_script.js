4858ffe33b390546c228efd54ebaaeab0c0b6d3e
Thomas GEORGE <thomas.george@itemm.fr>
2022-11-18 11:33:38 +0100

    working charts

--- js/p_w_script.js
+++ js/p_w_script.js
@@ -3,23 +3,15 @@
  * 
  * Basically it binds entry input and result output in the HTML.
  * 
- * If some constants need to be reset, please do it here.
+ * If some constants need to be reset, please do it the numConstants object below.
  * 
  */
 
 
 // ===== Numerical constants =====
 const numConstants = {
-    nuxy: 0.3,
-    nuyx: 0.03,
-    e1feuil: 12.9,
-    e2feuil: 1.9,
-    g12feuil: 1.49,
-    nulrres: 0.3,
-    nulrpers: 0.3,
-    nurlres: 0.05,
-    nurlpers: 0.05,
-    nultpers: 0.3,
+    nu12: 0.3,
+    nu21: 0.05,
 }
 
 // ===== Data I/O fields in plaques_woodhouse.html ===== 
@@ -54,33 +46,23 @@ const fields = [
     "vol",
     "mass",
     "mvol",
-    "nuxy",
-    "nuyx",
-    "e1feuil",
-    "e2feuil",
-    "g12feuil",
-    "nulrres",
-    "nulrpers",
-    "nurlres",
-    "nurlpers",
-    "nultpers",
-    "dr",
-    "dl",
+    "nu12",
+    "nu21",
+    "rf1",
+    "rf2",
     "d1",
-    "d2",
     "d3",
     "d4",
     "rrigi",
     "rgeo",
-    "e1pers",
-    "e2pers",
-    "g12pers",
-    "g12feuil",
     "dres",
     "e1res",
     "e2res",
     "g12res",
     "dfeuil",
+    "e1feuil",
+    "e2feuil",
+    "g12feuil",
     "dpers",
     "e1pers",
     "e2pers",
@@ -125,9 +107,12 @@ $j(document).ready(function () {
         }
     }
 
-    // ===== Calculations =====
+    //**************************
+    //****** Calculations ******
+    //**************************
     // ( !!! RESPECT ORDER OF CALCULATIONS !!!...)
 
+    // Renseigner les dimensions des plaques (upper part of chart)
     function calcVol() {
         // D12 = D9*D10*D11
         data.vol = data.long * data.larg * data.epai;
@@ -138,112 +123,191 @@ $j(document).ready(function () {
         data.mvol = data.mass / data.vol;
     }
 
+    // Valeurs de référence
     function calcDres() {
         // C33 = D14 / 1000
         data.dres = data.mvol / 1000;
-    }
-
-    function calcDfeuil () {
-        // C34 = D14 / 1000
-        data.dfeuil = data.mvol / 1000;
-    }
-    
-    function calcDpers() {
-        // C34 = D14 / 1000
-        data.dpers = data.mvol / 1000;
     }
 
     function calcE1res() {
         // D33 =(13000+45000*(C33-0,45))/1000
         data.e1res = (13000 + 45000 * (data.dres - 0.45)) / 1000;
     }
+
     function calcE2res() {
         // E33 =(1000+5500*(C33-0,45))/1000
         data.e2res = (1000 + 5500 * (data.dres - 0.45)) / 1000;
     }
+
     function calcG12res() {
         // G33 = =(840+1320*(C33-0,45))/1000
         data.g12res = (840 + 1320 * (data.dres - 0.45)) / 1000;
     }
 
+    function calcDfeuil() {
+        // C34 = D14 / 1000
+        data.dfeuil = data.mvol / 1000;
+    }
+
+    function calcE1feuil() {
+        //D34 = =(14200+21100*(C34-0,65))/1000
+        data.e1feuil = (14200 + 21100 * (data.dfeuil - 0.65)) / 1000;
+    }
+
+    function calcE2feuil() {
+        //E34 = =(2020+4340*(C34-0,65))/1000
+        data.e2feuil = (2020 + 4340 * (data.dfeuil - 0.65)) / 1000;
+    }
+
+    function calcG12feuil() {
+        //G34 = =(1025+2020*(C34-0,65))/1000
+        data.g12feuil = (1025 + 2020 * (data.dfeuil - 0.65)) / 1000;
+    }
+
+    function calcDpers() {
+        // C34 = D14 / 1000
+        data.dpers = data.mvol / 1000;
+    }
+
+    //Renseigner les fréquences des modes
+    // F torsion plan
     function calcFtres() {
-        //M3 =SQRT(((G33*1000000000/3)*D11*D11)/(0,274*D14*D9*D9*D10*D10))
-        data.ftres = Math.sqrt(((data.g12res * 1000000000 / 3) * data.epai * data.epai) / (0.27 * data.mvol * data.long * data.long * data.larg * data.larg));
+        //M3 =SQRT(((G33*1000000000/3)*D11*D11)/(0,4053*D14*D9*D9*D10*D10))
+        data.ftres = Math.sqrt(
+            (
+                (data.g12res * 1000000000 / 3) * data.epai * data.epai
+            ) / (
+                0.4053 * data.mvol * data.long * data.long * data.larg * data.larg
+            )
+        );
     }
 
     function calcFtfeuil() {
-        //N3 = =SQRT(((G34*1000000000/3)*D11*D11)/(0,274*D14*D9*D9*D10*D10))
-        data.ftfeuil = Math.sqrt(((data.g12feuil * 1000000000 / 3) * data.epai * data.epai) / (0.27 * data.mvol * data.long * data.long * data.larg * data.larg));
+        //N3 =SQRT(((G34*1000000000/3)*D11*D11)/(0,4053*D14*D9*D9*D10*D10))
+        data.ftfeuil = Math.sqrt(
+            (
+                (data.g12feuil * 1000000000 / 3) * data.epai * data.epai
+            ) / (
+                0.4053 * data.mvol * data.long * data.long * data.larg * data.larg
+            )
+        );
     }
 
     function calcFtpers() {
-        //O3 = =SQRT(((G35*1000000000/3)*D11*D11)/(0,274*D14*D9*D9*D10*D10))
-        data.ftpers = Math.sqrt(((data.g12pers * 1000000000 / 3) * data.epai * data.epai) / (0.27 * data.mvol * data.long * data.long * data.larg * data.larg));
+        //O3 =SQRT(((G35*1000000000/3)*D11*D11)/(0,4053*D14*D9*D9*D10*D10))
+        data.ftpers = Math.sqrt(
+            (
+                (data.g12pers * 1000000000 / 3) * data.epai * data.epai
+            ) / (
+                0.4053 * data.mvol * data.long * data.long * data.larg * data.larg
+            )
+        );
     }
 
-    ////////////////////////////////////////////
-    //DEBUG : need to check M4, D3 & E3 factor 10, D4, E4, E5, D6, G9, G11, G12, G13, D17, D18, D19 factor 10, D20, D21, G3 
+    // F flexion 1
     function calcFflexres1() {
-        //M4 = =SQRT(((D33*1000000000)/(12*(1-J33*K33))*(D11*D11))/(0,0789*D14*D9*D9*D9*D9))
-        data.fflexres1 = ((data.e1res * 1000000000) / (12 * (1 - data.nulrres * data.nurlres)) * (data.epai * data.epai)) / (0.0789 * data.mvol * Math.pow(data.long, 4));
-        // = Math.sqrt(((data.e1res * 1000000000) / (12 * (1 - data.nulrres * data.nurlres)) * (data.epai * data.epai)) / (0.0789 * data.mvol * Math.pow(data.long, 4)));
+        //M4 =SQRT(((D33*1000000000)/(12*(1-D15*D16))*(D11*D11))/(0,0789*D14*D9*D9*D9*D9))
+        data.fflexres1 = Math.sqrt(
+            (
+                (data.e1res * 1000000000) / (12 * (1 - data.nu12 * data.nu21))
+                * (data.epai * data.epai)
+            ) / (
+                0.0789 * data.mvol * Math.pow(data.long, 4)
+            )
+        );
     }
 
     function calcFflexfeuil1() {
-        //N4 = =SQRT(((D34*1000000000)/(12*(1-J33*K33))*(D11*D11))/(0,0789*D14*D9*D9*D9*D9))
-        data.fflexfeuil1 = Math.sqrt(((data.e1feuil * 1000000000) / (12 * (1 - data.nulrres * data.nurlres)) * (data.epai * data.epai)) / (0.0789 * data.mvol * Math.pow(data.long, 4)));
+        //N4 =SQRT(((D34*1000000000)/(12*(1-D15*D16))*(D11*D11))/(0,0789*D14*D9*D9*D9*D9))
+        data.fflexfeuil1 = Math.sqrt(
+            (
+                (data.e1feuil * 1000000000) / (12 * (1 - data.nu12 * data.nu21))
+                * (data.epai * data.epai)
+            ) / (
+                0.0789 * data.mvol * Math.pow(data.long, 4)
+            )
+        );
     }
 
     function calcFflexpers1() {
-        // O4 = =SQRT(((E35*1000000000)/(12*(1-K35*L35))*(D11*D11))/(0,0789*D14*D9*D9*D9*D9))
-        data.fflexpers1 = Math.sqrt(((data.e2pers * 1000000000) / (12 * (1 - data.nurlpers * data.nultpers)) * (data.epai * data.epai)) / (0.0789 * data.mvol * Math.pow(data.long, 4)));
+        //O4 =SQRT(((D35*1000000000)/(12*(1-D15*D16))*(D11*D11))/(0,0789*D14*D9*D9*D9*D9))
+        data.fflexpers1 = Math.sqrt(
+            (
+                (data.e1pers * 1000000000) / (12 * (1 - data.nu12 * data.nu21))
+                * (data.epai * data.epai)
+            ) / (
+                0.0789 * data.mvol * Math.pow(data.long, 4)
+            )
+        );
     }
+
+    // F flexion 2
     function calcFflexres2() {
-        //M5 = =SQRT(((E33*1000000000)/(12*(1-J33*K33))*(D11*D11))/(0,0789*D14*D10*D10*D10*D10))
-        data.fflexres2 = Math.sqrt(((data.e2res * 1000000000) / (12 * (1 - data.nulrres * data.nurlres)) * (data.epai * data.epai)) / (0.0789 * data.mvol * Math.pow(data.larg, 4)));
+        //M5 =SQRT(((E33*1000000000)/(12*(1-D15*D16))*(D11*D11))/(0,0789*D14*D10*D10*D10*D10))
+        data.fflexres2 = Math.sqrt(
+            (
+                (data.e2res * 1000000000) / (12 * (1 - data.nu12 * data.nu21))
+                * (data.epai * data.epai)
+            ) / (
+                0.0789 * data.mvol * Math.pow(data.larg, 4)
+            )
+        );
     }
 
     function calcFflexfeuil2() {
-        //N5 = =SQRT(((E34*1000000000)/(12*(1-J33*K33))*(D11*D11))/(0,0789*D14*D10*D10*D10*D10))
-        data.fflexfeuil2 = Math.sqrt(((data.e2feuil * 1000000000) / (12 * (1 - data.nulrres * data.nurlres)) * (data.epai * data.epai)) / (0.0789 * data.mvol * Math.pow(data.larg, 4)));
+        //N5 =SQRT(((E34*1000000000)/(12*(1-D15*D16))*(D11*D11))/(0,0789*D14*D10*D10*D10*D10))
+        data.fflexfeuil2 = Math.sqrt(
+            (
+                (data.e2feuil * 1000000000) / (12 * (1 - data.nu12 * data.nu21))
+                * (data.epai * data.epai)
+            ) / (
+                0.0789 * data.mvol * Math.pow(data.larg, 4)
+            )
+        );
     }
 
     function calcFflexpers2() {
-        // O5 = =SQRT(((D35*1000000000)/(12*(1-J35*K35))*(D11*D11))/(0,0789*D14*D10*D10*D10*D10))
-        data.fflexpers2 = Math.sqrt(((data.e1pers * 1000000000) / (12 * (1 - data.nulrpers * data.nurlpers)) * (data.epai * data.epai)) / (0.0789 * data.mvol * Math.pow(data.larg, 4)));
+        //O5 =SQRT(((E35*1000000000)/(12*(1-D15*D16))*(D11*D11))/(0,0789*D14*D10*D10*D10*D10))
+        data.fflexpers2 = Math.sqrt(
+            (
+                (data.e2pers * 1000000000) / (12 * (1 - data.nu12 * data.nu21))
+                * (data.epai * data.epai)
+            ) / (
+                0.0789 * data.mvol * Math.pow(data.larg, 4)
+            )
+        );
     }
-    ///////////////////////////////////////////////
 
+    // Renseigner les dimensions de plaques (lower part of chart)
     function calcD1() {
-        //D19 = =0,0789*L4*L4*D14*D9*D9*D9*D9/(D11*D11)
-        data.d1 = 0.789 * data.fflex1 * data.fflex1 * data.mvol * Math.pow(data.long, 4) / (data.epai * data.epai);
-    }
-    function calcD2() {
-        //D20 = =0,0789*L5*L5*D14*D10*D10*D10*D10/(D11*D11)
-        data.d2 = 0.789 * data.fflex2 * data.fflex2 * data.mvol * Math.pow(data.larg, 4) / (data.epai * data.epai);
-    }
-    function calcD3() {
-        //D21 = =0,0789*L5*L5*D14*D10*D10*D10*D10/(D11*D11)
-        data.d3 = 0.789 * data.fflex2 * data.fflex2 * data.mvol * Math.pow(data.larg, 4) / (data.epai * data.epai);
-    }
-    function calcD4() {
-        //D22 = =0,274*L3*L3*D14*D9*D9*D10*D10/(D11*D11)
-        data.d4 = 0.274 * data.ftplan * data.ftplan * data.mvol * data.long * data.long * data.larg * data.larg / (data.epai * data.epai);
+        //D19 =0,0789*L4*L4*D14*D9*D9*D9*D9/(D11*D11)
+        data.d1 = 0.0789 * data.fflex1 * data.fflex1 * data.mvol * Math.pow(data.long, 4) / (data.epai * data.epai);
     }
 
+    function calcD3() {
+        // D20 =0,0789*L5*L5*D14*D10*D10*D10*D10/(D11*D11)
+        data.d3 = 0.0789 * data.fflex2 * data.fflex2 * data.mvol * Math.pow(data.larg, 4) / (data.epai * data.epai);
+    }
+
+    function calcD4() {
+        //D21 =0,4053*L3*L3*D14*D9*D9*D10*D10/(D11*D11)
+        data.d4 = 0.4053 * data.ftplan * data.ftplan * data.mvol * data.long * data.long * data.larg * data.larg / (data.epai * data.epai);
+    }
+
+    // Rigidités - Modules spécifiques
     function calcElongr() {
-        //D3 = =D19*12*(1-J33*K33)*0,000000001
-        data.elongr = data.d1 * 12 * (1 - data.nulrres * data.nurlres) * 0.000000001;
+        // D3 =D19*12*(1-D15*D16)*0,000000001
+        data.elongr = data.d1 * 12 * (1 - data.nu12 * data.nu21) * 0.000000001;
     }
 
     function calcElongm() {
         //E3 = =D3/D14*1000
-        data.elongm = (data.elongr / data.mvol) * 1000
+        data.elongm = (data.elongr / data.mvol) * 1000;
     }
 
     function calcElargr() {
-        //D4 = =D21*12*(1-J33*K33)*0,000000001
-        data.elagr = data.d3 * 12 * (1 - data.nulrres * data.nurlres) * 0.000000001;
+        //D4 = =D20*12*(1-D15*D16)*0,000000001
+        data.elargr = data.d3 * 12 * (1 - data.nu12 * data.nu21) * 0.000000001;
     }
 
     function calcElargm() {
@@ -252,31 +316,35 @@ $j(document).ready(function () {
     }
 
     function calcGplanr() {
-        //D5 = =(D22*3*0,000000001)
+        //D5 = =(D21*3*0,000000001)
         data.gplanr = data.d4 * 3 * 0.000000001;
     }
 
     function calcGplanm() {
         //E5= =D5/D14*1000
+        data.gplanm = (data.gplanr / data.mvol) * 1000;
     }
 
     function calcRani() {
         //D6 =D4/D3
+        data.rani = data.elargr / data.elongr;
     }
 
-    function calcDr() {
-        // D17 = =D3*1000000000*D11*D11*D11/(12*(1-0,3*0,3))
-        data.dr = data.elongr * 1000000000 * Math.pow(data.epai, 3) / (12 * (1 - 0,3 * 0,3));
+    // Renseigner les dimensions de plaques (middle part of chart)
+    function calcRf1() {
+        // D17 = D3*1000000000*D11*D11*D11/(12*(1-0,3*0,3))
+        data.rf1 = data.elongr * 1000000000 * Math.pow(data.epai, 3) / (12 * (1 - 0.3 * 0.3));
     }
 
-    function calcDl() {
-        //D18 = =D4*1000000000*D11*D11*D11/(12*(1-0,3*0,3))
-        data.dl = data.elagr * 1000000000 * Math.pow(data.epai, 3) / (12 * (1 - 0,3 * 0,3));
+    function calcRf2() { 
+        //D18 =D4*1000000000*D11*D11*D11/(12*(1-0,3*0,3))
+        data.rf2 = data.elargr * 1000000000 * Math.pow(data.epai, 3) / (12 * (1 - 0.3 * 0.3));
     }
 
+    // two last calculations 
     function calcRrigi() {
         //G3 = =(D19/D20)^(0,25)
-        data.rrigi = Math.pow((data.d1 / data.d2), 4);
+        data.rrigi = Math.pow((data.d1 / data.d3), 0.25);
     }
 
     function calcRgeo() {
@@ -284,17 +352,20 @@ $j(document).ready(function () {
         data.rgeo = data.long / data.larg;
     }
 
-    
+
     // global 
     function calcChart() {
         calcVol();
         calcMvol();
         calcDres();
-        calcDfeuil();
-        calcDpers();
         calcE1res();
         calcE2res();
         calcG12res();
+        calcDfeuil();
+        calcE1feuil();
+        calcE2feuil();
+        calcG12feuil();
+        calcDpers();
         calcFtres();
         calcFtfeuil();
         calcFtpers();
@@ -305,7 +376,6 @@ $j(document).ready(function () {
         calcFflexfeuil2();
         calcFflexpers2();
         calcD1();
-        calcD2();
         calcD3();
         calcD4();
         calcElongr();
@@ -315,8 +385,8 @@ $j(document).ready(function () {
         calcGplanr();
         calcGplanm();
         calcRani();
-        calcDr();
-        calcDl();
+        calcRf1();
+        calcRf2();
         calcRrigi();
         calcRgeo();
 
