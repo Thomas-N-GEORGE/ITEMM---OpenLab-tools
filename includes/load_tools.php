265cc01eec4b06328b8762a90c76764a252d9aea
Thomas GEORGE <thomas.george@itemm.fr>
2022-11-22 14:09:37 +0100

    fixed bug in load_tools.php

--- includes/load_tools.php
+++ includes/load_tools.php
@@ -4,57 +4,57 @@
 function load_tool($tool) {
 
     // css common style sheet for all tools
-    wp_register_script('olt-script', plugin_dir_url( __DIR__ ) . '/css/olt_style.css');
-    wp_enqueue_style( 'olt-style',  plugin_dir_url( __DIR__ ) . '/css/olt_style.css' ); 
+    wp_register_style('olt_common_style', plugin_dir_url( __DIR__ ) . '/css/olt_style.css');
+    wp_enqueue_style( 'olt_common_style',  plugin_dir_url( __DIR__ ) . '/css/olt_style.css' ); 
     
     switch ($tool) {
         case "lames_idiophones":
             // css specific style sheets
-            wp_register_style('olt-style', plugin_dir_url( __DIR__ ) . '/css/l_i_style.css' );
-            wp_enqueue_style( 'olt-style',  plugin_dir_url( __DIR__ ) . '/css/l_i_style.css' ); 
+            wp_register_style('olt_l_i_style', plugin_dir_url( __DIR__ ) . '/css/l_i_style.css' );
+            wp_enqueue_style( 'olt_l_i_style',  plugin_dir_url( __DIR__ ) . '/css/l_i_style.css' ); 
             // js scripts
-            wp_register_script('olt-script', plugin_dir_url( __DIR__ ) . '/js/l_i_script.js', array('jquery'),'1.0', true);
-            wp_enqueue_script( 'olt-script',  plugin_dir_url( __DIR__ ) . '/js/l_i_script.js');
+            wp_register_script('olt_l_i_script', plugin_dir_url( __DIR__ ) . '/js/l_i_script.js', array('jquery'),'1.0', true);
+            wp_enqueue_script( 'olt_l_i_script',  plugin_dir_url( __DIR__ ) . '/js/l_i_script.js');
             // html
             include_once plugin_dir_path( __DIR__ ) . 'includes/lames_idiophones.html';
             break;
         case "flexion_3_points":
             // css specific style sheets
-            wp_register_style('olt-style', plugin_dir_url( __DIR__ ) . '/css/f_3_style.css' );
-            wp_enqueue_style( 'olt-style',  plugin_dir_url( __DIR__ ) . '/css/f_3_style.css' ); 
+            wp_register_style('olt_f_3_p_style', plugin_dir_url( __DIR__ ) . '/css/f_3_style.css' );
+            wp_enqueue_style( 'olt_f_3_p_style',  plugin_dir_url( __DIR__ ) . '/css/f_3_style.css' ); 
             // js scripts
-            wp_register_script('olt-script', plugin_dir_url( __DIR__ ) . '/js/f_3_script.js', array('jquery'),'1.0', true);
-            wp_enqueue_script( 'olt-script',  plugin_dir_url( __DIR__ ) . '/js/f_3_script.js');
+            wp_register_script('olt_f_3_p_script', plugin_dir_url( __DIR__ ) . '/js/f_3_script.js', array('jquery'),'1.0', true);
+            wp_enqueue_script( 'olt_f_3_p_script',  plugin_dir_url( __DIR__ ) . '/js/f_3_script.js');
             // html
             include_once plugin_dir_path( __DIR__ ) . 'includes/flexion_3_points.html';
             break;
         case "flexion_4_points":
             // css specific style sheets
-            wp_register_style('olt-style', plugin_dir_url( __DIR__ ) . '/css/f_4_style.css' );
-            wp_enqueue_style( 'olt-style',  plugin_dir_url( __DIR__ ) . '/css/f_4_style.css' ); 
+            wp_register_style('olt_f_4_p_style', plugin_dir_url( __DIR__ ) . '/css/f_4_style.css' );
+            wp_enqueue_style( 'olt_f_4_p_style',  plugin_dir_url( __DIR__ ) . '/css/f_4_style.css' ); 
             // js scripts
-            wp_register_script('olt-script', plugin_dir_url( __DIR__ ) . '/js/f_4_script.js', array('jquery'),'1.0', true);
-            wp_enqueue_script( 'olt-script',  plugin_dir_url( __DIR__ ) . '/js/f_4_script.js');
+            wp_register_script('olt_f_4_p_script', plugin_dir_url( __DIR__ ) . '/js/f_4_script.js', array('jquery'),'1.0', true);
+            wp_enqueue_script( 'olt_f_4_p_script',  plugin_dir_url( __DIR__ ) . '/js/f_4_script.js');
             // html                    
             include_once plugin_dir_path( __DIR__ ) . 'includes/flexion_4_points.html';
             break;
         case "poutres" :
             // css specific style sheets
-            wp_register_style('olt-style', plugin_dir_url( __DIR__ ) . '/css/poutres_style.css' );
-            wp_enqueue_style( 'olt-style',  plugin_dir_url( __DIR__ ) . '/css/poutres_style.css' ); 
+            wp_register_style('olt_poutres_style', plugin_dir_url( __DIR__ ) . '/css/poutres_style.css' );
+            wp_enqueue_style( 'olt_poutres_style',  plugin_dir_url( __DIR__ ) . '/css/poutres_style.css' ); 
             // js scripts
-            wp_register_script('olt-script', plugin_dir_url( __DIR__ ) . '/js/poutres_script.js', array('jquery'),'1.0', true);
-            wp_enqueue_script( 'olt-script',  plugin_dir_url( __DIR__ ) . '/js/poutres_script.js');
+            wp_register_script('olt_poutres_script', plugin_dir_url( __DIR__ ) . '/js/poutres_script.js', array('jquery'),'1.0', true);
+            wp_enqueue_script( 'olt_poutres_script',  plugin_dir_url( __DIR__ ) . '/js/poutres_script.js');
             // html                    
             include_once plugin_dir_path( __DIR__ ) . 'includes/poutres.html';
             break;
         case "plaques_woodhouse" :
             // css specific style sheets
-            wp_register_style('olt-style', plugin_dir_url( __DIR__ ) . '/css/p_w_style.css' );
-            wp_enqueue_style( 'olt-style',  plugin_dir_url( __DIR__ ) . '/css/p_w_style.css' ); 
+            wp_register_style('olt_p_w_style', plugin_dir_url( __DIR__ ) . '/css/p_w_style.css' );
+            wp_enqueue_style( 'olt_p_w_style',  plugin_dir_url( __DIR__ ) . '/css/p_w_style.css' ); 
             // js scripts
-            wp_register_script('olt-script', plugin_dir_url( __DIR__ ) . '/js/p_w_script.js', array('jquery'),'1.0', true);
-            wp_enqueue_script( 'olt-script',  plugin_dir_url( __DIR__ ) . '/js/p_w_script.js');
+            wp_register_script('olt_p_w_script', plugin_dir_url( __DIR__ ) . '/js/p_w_script.js', array('jquery'),'1.0', true);
+            wp_enqueue_script( 'olt_p_w_script',  plugin_dir_url( __DIR__ ) . '/js/p_w_script.js');
             // html                    
             include_once plugin_dir_path( __DIR__ ) . 'includes/plaques_woodhouse.html';
             break;
